8f84ba80fd0daaf9669ef38b3554456e3ca4cc9c
Norbi <szapu.norbert@gs.hu>
2024-10-11 13:09:14 +0200

    End of Section 9

---
+++ API/Data/Migrations/20241011105046_UpdatedUserEntity2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedUserEntity2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LookinFor",
+                table: "Users",
+                newName: "LookingFor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LookingFor",
+                table: "Users",
+                newName: "LookinFor");
+        }
+    }
+}
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -24,7 +24,7 @@ export const routes: Routes = [
         component: MemberListComponent,
       },
       {
-        path: 'member/:id',
+        path: 'members/:username',
         component: MemberDetailComponent,
       },
       {
